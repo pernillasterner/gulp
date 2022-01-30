9b767a397c18fb9bdfb9fb9642937e48e5fd3cc0
Pernilla Sterner <pernilla@techlove.se>
2022-01-30 09:43:49 +0100

    Add gulpfile and packages

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Setting up Gulp & Sass & Browser Sync</title>
+    <!-- https://coder-coder.com/quick-guide-to-browsersync-gulp-4/ -->
+    <title>Setting up Gulp, Browser Sync & Sass</title>
 </head>
 <body>
     <h1>How to set up Gulp, Sass and Browser Sync</h1>
+
+    <ul>
+        <li>node -v</li>
+        <li>npm -v</li>
+        <li>npm install gulp-cli -g</li>
+        <li>npm init -y</li>
+        <br>
+        <hr>
+        <h4>Installing packages</h4>
+        <li>npm install gulp gulp-sass gulp-postcss cssnano gulp-terser browser-sync</li>
+        <li>create gulpfile.js to add all the configuration</li>
+    </ul>
 </body>
 </html>
